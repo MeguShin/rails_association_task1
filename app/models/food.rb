b1582c8f4e679f685feef2f9003574a71245810f
shin <megushin0715@gmail.com>
2023-06-08 21:08:05 +0900

    second

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,5 @@
 class Food < ApplicationRecord
   has_many :order_foods
-  has_many :japanese_foods
-  has_many :italian_foods
-  has_many :cheinese_foods
   belongs_to :shop
+  belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_foods
-  belongs_to :orders
+  has_many :foods
+  belongs_to :address
+  belongs_to :customer
 end
